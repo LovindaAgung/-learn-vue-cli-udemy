5282fde43ba211622f83c902607489c01572bd5f
Lovinda Agung <lovinda.agung@gmail.com>
2021-11-18 12:59:04 +0700

    add friend-contact component; add style;

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,36 @@
+<template>
+    <li>
+        <header><h1>{{friend.name}}</h1></header>
+        <button @click="toggleDetails">{{visibilityStatus}} Detail</button>
+    </li>
+    <ul v-show="detailVisible">
+        <li>phone: {{friend.phone}}</li>
+        <li>Email: {{friend.email}}</li>
+    </ul>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            friend:{
+                    id:'alif',
+                    name: 'alif raher',
+                    phone: '0891234567890',
+                    email: 'alif@gmail.com',
+            },
+            detailVisible:false,
+        };
+    },
+    computed: {
+        visibilityStatus(){
+            return this.detailVisible ? "Hide" : "Show";
+        }
+    },
+    methods:{
+        toggleDetails(){
+            this.detailVisible = !this.detailVisible;
+        }
+    }
+};
+</script>
